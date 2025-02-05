2b12d91b4cfecc1cab2af3ef19daf1f29b5af527
KazumaShiigai <shiigai@effective-inc.com>
2025-02-05 13:12:24 +0900

    Todo登録・編集・削除

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { getCurrentUser } from "aws-amplify/auth";
 import { generateClient } from 'aws-amplify/api';
 import "@aws-amplify/ui-react/styles.css";
 import {
@@ -16,12 +17,34 @@ import {
   View,
 } from "@aws-amplify/ui-react";
 import { listTodos } from "./graphql/queries";
+import {
+  createTodo as createTodoMutation,
+  updateTodo as updateTodoMutation,
+  deleteTodo as deleteTodoMutation,
+} from "./graphql/mutations";
 import React, { useState, useEffect } from 'react';
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AddTodoModal } from "./components/AddTodoModal";
+import { EditTodoModal } from "./components/EditTodoModal";
 
 const client = generateClient();
 
 function App({ signOut}) {
+  const [user, setUser]   = useState({ id: "", name: ""});
   const [todos, setTodos] = useState([]);
+  const [preTodo, setPreTodo] = useState([]);
+  const [isOpenAddModal, setIsOpenAddModal]   = useState(false);
+  const [isOpenEditModal, setIsOpenEditModal] = useState(false);
+
+  useEffect(() => {
+    const fetchCurrentUser = async () => {
+      const userInfo = await getCurrentUser();
+      // userInfo.userId: ユーザーID(Sub), userInfo.username: ユーザー名
+      setUser({ id: userInfo.userId, name: userInfo.username });
+    }
+    fetchCurrentUser();
+  }, []);
 
   useEffect(() => {
     fetchTodos();
@@ -33,49 +56,135 @@ function App({ signOut}) {
     setTodos(todosFromAPI);
   }
 
+  function handleAddModal() {
+    setIsOpenAddModal(!isOpenAddModal);
+  }
+
+  function handleEditModal() {
+    setIsOpenEditModal(!isOpenEditModal);
+  }
+
+  async function createTodo(event) {
+    const data = {
+      title: event.title,
+      status: event.status,
+      priority: event.priority,
+      start: event.start,
+      end: event.end,
+      description: event.description,
+      user: event.user,
+    };
+    await client.graphql({ 
+      query: createTodoMutation, 
+      variables: { input: data } 
+    });
+    fetchTodos();
+  }
+
+  async function updateTodo(event) {
+    const data = {
+      id: event.id,
+      title: event.title,
+      status: event.status,
+      priority: event.priority,
+      start: event.start,
+      end: event.end,
+      description: event.description,
+      user: event.user,
+    }
+    await client.graphql({
+      query: updateTodoMutation,
+      variables: { input: data }
+    });
+    fetchTodos();
+  }
+
+  async function deleteTodo({ id }) {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+    await client.graphql({ 
+      query: deleteTodoMutation, 
+      variables: { input: { id } } 
+    });
+  }
+
   return (
-    <View className="App" margin="0.3rem">
-      <View className="header">
-        <Flex
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
-        > 
-          <Heading level={1}>My Todo App</Heading>
-          <Flex direction="row">
-            <Button onClick={signOut}>Sign Out</Button>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <View className="App" margin="0.3rem">
+        <View className="header">
+          <Flex
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+          > 
+            <Heading level={1}>My Todo App</Heading>
+            <Flex direction="row">
+              <p>{user.name}</p>
+              <Button onClick={signOut}>Sign Out</Button>
+            </Flex>
           </Flex>
-        </Flex>
-      </View>
-      <View margin="3rem 0">
-        <Table caption="" highlightOnHover={false}>
-          <TableHead>
-            <TableRow>
-              <TableCell as="th">No.</TableCell>
-              <TableCell as="th">Title</TableCell>
-              <TableCell as="th">Status</TableCell>
-              <TableCell as="th">Priority</TableCell>
-              <TableCell as="th">Milestone</TableCell>
-              <TableCell as="th">Discription</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {todos.map((todo, index) => (
-              <TableRow key={index}>
-                <TableCell>{index}</TableCell>
-                <TableCell>{todo.title}</TableCell>
-                <TableCell>{todo.status}</TableCell>
-                <TableCell>{todo.priority}</TableCell>
-                <TableCell>
-                  {todo.start} - {todo.end}
-                </TableCell>
-                <TableCell>{todo.description ? todo.description : "-"}</TableCell>
+        </View>
+        <View className="add-todo-button" margin="1rem 1rem">
+          <Flex direction="row" justifyContent="space-between" alignItems="center">
+            <Button onClick={handleAddModal}>Add +</Button>
+          </Flex>
+        </View>
+        {isOpenAddModal && (
+          <AddTodoModal 
+            userId={user.id} 
+            createTodo={createTodo} 
+            handleAddModal={handleAddModal}>
+          </AddTodoModal>
+        )}
+        {isOpenEditModal && (
+          <EditTodoModal 
+            userId={user.id} 
+            preTodo={preTodo} 
+            updateTodo={updateTodo}
+            handleEditModal={handleEditModal}>
+          </EditTodoModal>
+        )}
+        <View margin="3rem 0">
+          <Table caption="" highlightOnHover={false}>
+            <TableHead>
+              <TableRow>
+                <TableCell as="th">No.</TableCell>
+                <TableCell as="th">Title</TableCell>
+                <TableCell as="th">Status</TableCell>
+                <TableCell as="th">Priority</TableCell>
+                <TableCell as="th">Milestone</TableCell>
+                <TableCell as="th">Discription</TableCell>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+            </TableHead>
+            <TableBody>
+              {todos.map((todo, index) => (
+                <TableRow key={index}>
+                  <TableCell>{index}</TableCell>
+                  <TableCell>{todo.title}</TableCell>
+                  <TableCell>{todo.status}</TableCell>
+                  <TableCell>{todo.priority}</TableCell>
+                  <TableCell>
+                    {todo.start} - {todo.end}
+                  </TableCell>
+                  <TableCell>
+                    {todo.description ? todo.description : "-"}
+                  </TableCell>
+                  <TableCell>
+                    <Button 
+                      margin="0 1rem"
+                      onClick={() => {
+                        setPreTodo(todo);
+                        handleEditModal();
+                      }}>Edit</Button>
+                    <Button onClick={() => deleteTodo(todo)}>Delete</Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </View>
       </View>
-    </View>
+    </LocalizationProvider>
   );
 }
 
---
+++ src/components/AddTodoModal.js
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import { 
+    Button, 
+    Flex, 
+    Heading, 
+    SelectField, 
+    TextField, 
+    TextAreaField, 
+    View, 
+} from "@aws-amplify/ui-react";
+import {
+    DatePicker
+} from "@mui/x-date-pickers";
+import dayjs from "dayjs";
+
+const statusList   = ["New", "Open", "On Hold", "Resolved", "Close"];
+const priorityList = ["Trivial", "Minor", "Major", "Critical", "Blocker"];
+
+export function AddTodoModal({ userId, createTodo, handleAddModal }) {
+    const [todo, setTodos] = useState({
+        user: userId,
+        title: undefined,
+        status: statusList[0],
+        priority: priorityList[0],
+        start: undefined,
+        end: undefined,
+        description: undefined,
+    });
+
+    return (
+        <div className="overlay">
+            <div className="content">
+                <View className="header">
+                    <Heading level={2}>Add Todo</Heading>
+                </View>
+                <View margin="1rem">
+                    <Flex direction="column">
+                        <TextField
+                            placeholder="Input todo title"
+                            label="Title"
+                            onChange={(e) => setTodos({ ...todo, title: e.target.value })}
+                        />
+                        <TextAreaField
+                            placeholder="Input todo description"
+                            label="Description"
+                            onChange={(e) => setTodos({ ...todo, description: e.target.value })}
+                        />
+                        <SelectField
+                            label="Status"
+                            onChange={(e) => setTodos({ ...todo, status: e.target.value })}
+                        >
+                            {statusList.map((item, index) => (
+                                <option key={index} value={item}>{item}</option>
+                            ))}
+                        </SelectField>
+                        <SelectField
+                            label="Priority"
+                            onChange={(e) => setTodos({ ...todo, priority: e.target.value })}
+                        >
+                            {priorityList.map((item, index) => (
+                                <option key={index} value={item}>{item}</option>
+                            ))}
+                        </SelectField>
+                        <Flex 
+                            direction="row" 
+                            justifyContent="space-between" 
+                            alignItems="center" 
+                            margin="1rem 0"
+                        >
+                            <DatePicker
+                                label="Start Date"
+                                onChange={(date) => setTodos({ ...todo, start: dayjs(date).format("YYYY-MM-DD") })}
+                            />
+                            <span>-</span>
+                            <DatePicker
+                                label="End Date"
+                                onChange={(date) => setTodos({ ...todo, end: dayjs(date).format("YYYY-MM-DD") })}
+                            />
+                        </Flex>
+                    </Flex>
+                    <Flex 
+                        direction="row" 
+                        justifyContent="flex-end" 
+                        alignItems="center" 
+                        margin="1rem 0"
+                    >
+                        <Button onClick={handleAddModal}>Cansel</Button>
+                        <Button 
+                            onClick={() => {
+                                createTodo(todo);
+                                handleAddModal();
+                            }}
+                        >
+                            Add
+                        </Button>
+                    </Flex>
+                </View>
+            </div>
+        </div>
+    );
+}
